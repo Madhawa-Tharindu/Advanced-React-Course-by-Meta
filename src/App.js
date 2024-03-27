ba0b57924def64fc479f97fd8e25bac8c2409bb5
Madhawa-Tharindu <madhawath@gmail.com>
2024-03-28 05:04:04 +0530

    form creation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
 
 function App() {
-  return (
+
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setName("");
+    console.log("Form Submitted!", name);
+  }
+    return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +26,25 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      
+      <form onSubmit={handleSubmit}>
+         <fieldset>
+          <div className="Field">
+              <label htmlFor='name'>Name:</label>
+              <input 
+                id='name'
+                type="text"
+                placeholder="Name"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                />
+          </div>
+          <button disabled={!name} type="submit">Submit</button>
+         </fieldset>
+      </form>
+
     </div>
   );
 }
